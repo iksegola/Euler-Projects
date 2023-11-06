e4072a2a241c639d701b903d012046c7820a9cd7
Ingrid Segola <215274535@tut4life.ac.za>
2023-11-06 22:30:17 +0200

    added numbers

--- Problem17.py
+++ Problem17.py
@@ -1,2 +1,15 @@
 #Problem 17
 #The problem of counting numbers
+
+numbers  = ['one', 'two', 'three', 'four', 'five']
+
+nums_as_dict = {1: 'one',
+                2: 'two',
+                3:'three',
+                4:'four',
+                5:'five'
+
+}
+
+length = len(nums_as_dict)
+print (length)
